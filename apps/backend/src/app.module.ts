8a50a4e6244816cc65d7ee099d6643b20bfdad84
Franz Nunez <kasparov01234@gmail.com>
2026-03-11 10:46:00 -0500

    feat: Configuring Prisma with PostgreSQL

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [AdminModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
