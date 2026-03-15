83ef6f7632bcc621ca1ce3647fcb187377350550
Franz Nunez <kasparov01234@gmail.com>
2026-03-14 22:21:15 -0500

    Configuring database in english

--- apps/backend/src/admin/courses/dto/create-course.dto.ts
+++ apps/backend/src/admin/courses/dto/create-course.dto.ts
@@ -13,34 +13,34 @@ export class CreateCourseDto {
   @IsNotEmpty({ message: 'El nombre del curso es obligatorio' })
   @MinLength(5, { message: 'El nombre del curso debe tener al menos 5 caracteres' })
   @MaxLength(100, { message: 'El nombre del curso no puede exceder los 100 caracteres' })
-  nombre: string;
+  name: string;
 
   @IsString()
   @IsNotEmpty({ message: 'El código del curso es obligatorio' })
   @MaxLength(10, { message: 'El código del curso no puede exceder los 10 caracteres' })
-  codigo_curso: string;
+  course_code: string;
 
   @IsString()
   @IsNotEmpty({ message: 'La descripción del curso es obligatoria' })
-  descripcion: string;
+  description: string;
 
   @IsUrl({}, { message: 'La URL de la imagen debe ser una URL válida' })
   @IsOptional()
-  url_imagen?: string;
+  url_image?: string;
 
   @IsInt({ message: 'El ID del docente debe ser un número entero' })
   @IsOptional()
-  id_docente?: number;
+  teacher_id?: number;
 
   @IsString()
   @IsOptional()
   @MaxLength(50, { message: 'El nombre del docente no puede exceder los 50 caracteres' })
-  nombre_docente?: string;
+  teacher_name?: string;
 
   @IsString()
   @IsOptional()
   @MaxLength(50, {
     message: 'El apellido del docente no puede exceder los 50 caracteres',
   })
-  apellido_docente?: string;
+  teacher_last_name?: string;
 }
