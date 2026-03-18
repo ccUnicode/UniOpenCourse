b6196600cf7e31717e9c995c354c1bc5e2536ac0
Franz Nunez <kasparov01234@gmail.com>
2026-03-18 11:10:30 -0500

    Fix: Updating imports and dependencies

--- apps/backend/src/admin/classes/classes.controller.ts
+++ apps/backend/src/admin/classes/classes.controller.ts
@@ -1,37 +1,34 @@
-import {Post, Get, Patch, Delete, Body, Param} from '@nestjs/common';
+import { Post, Get, Patch, Delete, Body, Param } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
 import { ClassesService } from './classes.service';
-<<<<<<< HEAD
-=======
-import {CreateClassDto} from './dto/create-class.dto';
-import {UpdateClassDto} from './dto/update-class.dto';
->>>>>>> 48e231dc6f2b9d8736bdc3cec2df6d8452ae0831
+import { CreateClassDto } from './dto/create-class.dto';
+import { UpdateClassDto } from './dto/update-class.dto';
 
 @Controller('admin/classes')
 export class ClassesController {
-    constructor (private readonly service: ClassesService) {}
-    @Post()
-    create(@Body() createClassDto: CreateClassDto) {
+  constructor(private readonly service: ClassesService) {}
+  @Post()
+  create(@Body() createClassDto: CreateClassDto) {
     return this.service.create(createClassDto);
-    }
+  }
 
-    @Get()
-    findAll() {
+  @Get()
+  findAll() {
     return this.service.findAll();
-    }     
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
+  @Get(':id')
+  findOne(@Param('id') id: string) {
     return this.service.findOne(+id);
-    }
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateClassDto: UpdateClassDto) {
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateClassDto: UpdateClassDto) {
     return this.service.update(+id, updateClassDto);
-    }
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
+  @Delete(':id')
+  remove(@Param('id') id: string) {
     return this.service.remove(+id);
-    }
+  }
 }
--- apps/backend/src/admin/classes/classes.service.ts
+++ apps/backend/src/admin/classes/classes.service.ts
@@ -1,38 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../../prisma/prisma.service';
-import {CreateClassDto} from './dto/create-class.dto';
+import { PrismaService } from 'src/prisma.service';
+import { CreateClassDto } from './dto/create-class.dto';
 import { UpdateClassDto } from './dto/update-class.dto';
 @Injectable()
 export class ClassesService {
-    constructor (private prisma: PrismaService) {};
-    async create(createClassDto: CreateClassDto) {
+  constructor(private prisma: PrismaService) {}
+  async create(createClassDto: CreateClassDto) {
     return this.prisma.class.create({
-    data: createClassDto,
+      data: createClassDto,
     });
-    }
+  }
 
-    async findAll() {
+  async findAll() {
     return this.prisma.class.findMany();
-    }
+  }
 
-    async findOne(id: number) {
+  async findOne(id: number) {
     return this.prisma.class.findUnique({
-    where: { class_id: id },
+      where: { class_id: id },
     });
-    }
+  }
 
-    async update(id: number, updateClassDto: UpdateClassDto) {
+  async update(id: number, updateClassDto: UpdateClassDto) {
     return this.prisma.class.update({
-    where: { class_id: id },
-    data: updateClassDto,
+      where: { class_id: id },
+      data: updateClassDto,
     });
-    }
+  }
 
-    async remove(id: number) {
+  async remove(id: number) {
     return this.prisma.class.delete({
-    where: { class_id: id },
+      where: { class_id: id },
     });
-    }
+  }
 }
-
-
