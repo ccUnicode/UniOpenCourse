a7ac6ff1002edba6815204fd95eee8fcc01cab19
Franz Nunez <kasparov01234@gmail.com>
2026-05-02 08:42:01 -0500

    fix: fixing twice import of ValidationPipe

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,8 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-import { ValidationPipe } from '@nestjs/common';
-
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
