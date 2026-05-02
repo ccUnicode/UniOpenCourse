562eb72ea45df7027387520623c0f43418c61612
Franz Nunez <kasparov01234@gmail.com>
2026-05-02 10:21:48 -0500

    chore: importing PrismaModule in global-searcher.module

--- apps/backend/src/global-searcher/global-searcher.module.ts
+++ apps/backend/src/global-searcher/global-searcher.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GlobalSearcherController } from './global-searcher.controller';
 import { GlobalSearcherService } from './global-searcher.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [GlobalSearcherController],
-  providers: [GlobalSearcherService]
+  providers: [GlobalSearcherService],
 })
 export class GlobalSearcherModule {}
