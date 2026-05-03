ae0aba116e2309a733159c8f6b2eb1276084b975
Franz Nunez <kasparov01234@gmail.com>
2026-05-03 00:35:01 -0500

    Organizing imports in app module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,26 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
+import { ClassesModule } from './classes/classes.module';
 import { CoursesModule } from './courses/courses.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { GlobalSearcherModule } from './global-searcher/global-searcher.module';
-import { ClassesModule } from './classes/classes.module';
 import { MaterialsModule } from './materials/materials.module';
-import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
   imports: [
     PrismaModule,
     AdminModule,
+    AuthModule,
+    ClassesModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     CoursesModule,
-    PrismaModule,
     GlobalSearcherModule,
-    ClassesModule,
     MaterialsModule,
-    AuthModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
