d8c53525370fa9cc69537264fad509ead13d3881
Franz Nunez <kasparov01234@gmail.com>
2026-05-03 00:18:05 -0500

    feat: Formating return of global searcher

--- apps/backend/src/global-searcher/global-searcher.service.ts
+++ apps/backend/src/global-searcher/global-searcher.service.ts
@@ -15,68 +15,87 @@ export class GlobalSearcherService {
     const safePage = Math.min(MAX_PAGE, page);
     const offset = (safePage - 1) * pageSize;
 
-    // Contar el total de resultados para cursos y clases
-    const TotalCourses = await this.prisma.course.count({
-      where: {
-        name: {
-          contains: q,
-          mode: 'insensitive', // Búsqueda insensible a mayúsculas
+    const [TotalCourses, TotalClasses, courses, classes] = await Promise.all([
+      this.prisma.course.count({
+        where: {
+          name: {
+            contains: q,
+            mode: 'insensitive', // Búsqueda insensible a mayúsculas
+          },
         },
-      },
-    });
-    const TotalClasses = await this.prisma.class.count({
-      where: {
-        title: {
-          contains: q,
-          mode: 'insensitive', // Búsqueda insensible a mayúsculas
+      }),
+      this.prisma.class.count({
+        where: {
+          title: {
+            contains: q,
+            mode: 'insensitive',
+          },
         },
-      },
-    });
+      }),
+      this.prisma.course.findMany({
+        where: {
+          OR: [
+            {
+              name: {
+                contains: q,
+                mode: 'insensitive',
+              },
+            },
+            {
+              course_code: {
+                contains: q,
+                mode: 'insensitive',
+              },
+            },
+          ],
+        },
+        skip: offset,
+        take: pageSize,
+        include: {
+          teacher: true,
+        },
+      }),
+      this.prisma.class.findMany({
+        where: {
+          title: {
+            contains: q,
+            mode: 'insensitive',
+          },
+        },
+        skip: offset,
+        take: pageSize,
+        include: {
+          course: true,
+        },
+      }),
+    ]);
 
     const totalResults = TotalCourses + TotalClasses;
     const totalPages = Math.ceil(totalResults / pageSize);
 
-    const courses = await this.prisma.course.findMany({
-      where: {
-        OR: [
-          {
-            name: {
-              contains: q,
-              mode: 'insensitive', // Búsqueda insensible a mayúsculas
-            },
-          },
-          {
-            course_code: {
-              contains: q,
-              mode: 'insensitive', // Búsqueda insensible a mayúsculas
-            },
-          },
-        ],
-      },
-      skip: offset,
-      take: pageSize,
-      include: {
-        teacher: true,
-      },
-    });
-    const classes = await this.prisma.class.findMany({
-      where: {
-        title: {
-          contains: q,
-          mode: 'insensitive', // Búsqueda insensible a mayúsculas
+    const data = [
+      ...courses.map((course) => ({
+        type: 'course',
+        course_id: course.course_id,
+        name: course.name,
+        url_image: course.url_image,
+        description: course.description,
+        course_code: course.course_code,
+        teacher: {
+          id: course.teacher.teacher_id,
+          name: course.teacher.name,
         },
-      },
-      skip: offset,
-      take: pageSize,
-      include: {
-        course: true,
-      },
-    });
+      })),
+      ...classes.map((cls) => ({
+        type: 'class',
+        class_id: cls.class_id,
+        title: cls.title,
+        course: { course_id: cls.course.course_id, name: cls.course.name },
+        url_youtube: cls.url_youtube,
+      })),
+    ];
     return {
-      data: {
-        courses,
-        classes,
-      },
+      data: data,
       page,
       totalPages,
       totalResults,
