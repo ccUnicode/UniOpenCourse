3fe0506a7b37cafe5bad8c57ca83416275e7639c
Franz Nunez <kasparov01234@gmail.com>
2026-05-21 16:14:53 -0500

    refactor: Adding forbidNonWhitelisted in main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       transform: true,
+      forbidNonWhitelisted: true,
     }),
   );
   await app.listen(process.env.PORT ?? 3001);
