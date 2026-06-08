94e4ef137c318e87c214bf13ecb05978f3b88851
Franz Nunez <kasparov01234@gmail.com>
2026-06-08 00:36:29 -0500

    feat: adding structure for course page

--- apps/frontend/app/cursos/[course_id]/page.tsx
+++ apps/frontend/app/cursos/[course_id]/page.tsx
@@ -1,3 +1,16 @@
-export default function Curso() {
-  return <h1>Curso 1</h1>;
+const baseUrl = process.env.API_URL || 'http://localhost:3001';
+async function getCourse(course_id: string) {
+  let response = await fetch(`${baseUrl}/courses/${course_id}`);
+  let course = await response.json();
+  return course;
+}
+
+export default async function Curso({
+  params,
+}: {
+  params: Promise<{ course_id: string }>;
+}) {
+  const { course_id } = await params;
+  const course = await getCourse(course_id);
+  return <h1>{course.name}</h1>;
 }
