2496e47c57fc9ed1194de489c2e6cf17eb7076cc
Franz Nunez <kasparov01234@gmail.com>
2026-06-14 10:41:36 -0500

    feat: adding materials to class page details

--- apps/frontend/app/cursos/[course_id]/clases/[class_id]/page.tsx
+++ apps/frontend/app/cursos/[course_id]/clases/[class_id]/page.tsx
@@ -1,4 +1,11 @@
 const baseUrl = process.env.API_URL || 'http://localhost:3001';
+
+async function getMaterialData(class_id: string) {
+  let response = await fetch(`${baseUrl}/classes/${class_id}/materials`);
+  let materials = await response.json();
+  return materials;
+}
+
 async function getClassData(class_id: string) {
   let response = await fetch(`${baseUrl}/classes/${class_id}`);
   let clase = await response.json();
@@ -11,6 +18,7 @@ export default async function Clase({
 }) {
   const { class_id } = await params;
   const clase = await getClassData(class_id);
+  const materials = await getMaterialData(class_id);
 
   return (
     <>
@@ -22,6 +30,12 @@ export default async function Clase({
         src={clase.url_youtube} // Asegurar que el link sea youtube embed
         title="YouTube video player"
       ></iframe>
+      <h2>Materials</h2>
+      <ul>
+        {materials.map((material: any) => (
+          <li key={material.material_id}>{material.filename} </li>
+        ))}
+      </ul>
     </>
   );
 }
