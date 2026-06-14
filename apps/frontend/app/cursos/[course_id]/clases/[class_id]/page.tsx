e7254759e67a9b547d377950a76e2fc713915ad6
Franz Nunez <kasparov01234@gmail.com>
2026-06-14 10:37:58 -0500

    feat: adding class page details

--- apps/frontend/app/cursos/[course_id]/clases/[class_id]/page.tsx
+++ apps/frontend/app/cursos/[course_id]/clases/[class_id]/page.tsx
@@ -1,3 +1,27 @@
-export default function Clase() {
-  return <h1>Clase 1</h1>;
+const baseUrl = process.env.API_URL || 'http://localhost:3001';
+async function getClassData(class_id: string) {
+  let response = await fetch(`${baseUrl}/classes/${class_id}`);
+  let clase = await response.json();
+  return clase;
+}
+export default async function Clase({
+  params,
+}: {
+  params: Promise<{ class_id: string }>;
+}) {
+  const { class_id } = await params;
+  const clase = await getClassData(class_id);
+
+  return (
+    <>
+      <h1>{clase.title}</h1>
+      <p>{clase.description}</p>
+      <iframe
+        width="560"
+        height="315"
+        src={clase.url_youtube} // Asegurar que el link sea youtube embed
+        title="YouTube video player"
+      ></iframe>
+    </>
+  );
 }
