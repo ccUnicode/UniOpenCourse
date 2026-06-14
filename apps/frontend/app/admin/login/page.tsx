420b112cb13ef9a0e037cdf66681347d7b364901
Franz Nunez <kasparov01234@gmail.com>
2026-06-14 11:13:46 -0500

    feat: adding structure for admin login

--- apps/frontend/app/admin/login/page.tsx
+++ apps/frontend/app/admin/login/page.tsx
@@ -1,3 +1,24 @@
+'use client';
+const baseUrl = process.env.API_URL || 'http://localhost:3001';
+
+const handleSubmit = async (
+  event: React.SyntheticEvent<HTMLFormElement, SubmitEvent>,
+) => {
+  event.preventDefault();
+  // Utilizar funcion fetch para enviar datos al backend (/auth/admin/login)
+};
+
 export default function LoginAdmin() {
-  return <h1>Iniciar sesión Admin</h1>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Correo Electrónico:</label>
+        <input type="email" id="email" name="email" required />
+        <label htmlFor="password">Contraseña:</label>
+        <input type="password" id="password" name="password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
