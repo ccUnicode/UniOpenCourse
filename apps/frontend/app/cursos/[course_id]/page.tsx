3f9ae4e60eb46533994eb5b17c5d486556962acc
Franz Nunez <kasparov01234@gmail.com>
2026-06-14 10:23:50 -0500

    feat: adding class list to course page

--- apps/frontend/app/cursos/[course_id]/page.tsx
+++ apps/frontend/app/cursos/[course_id]/page.tsx
@@ -1,4 +1,11 @@
 const baseUrl = process.env.API_URL || 'http://localhost:3001';
+
+async function getClassesFromCourse(course_id: string) {
+  let response = await fetch(`${baseUrl}/courses/${course_id}/classes`);
+  let classes = await response.json();
+  return classes;
+}
+
 async function getCourse(course_id: string) {
   let response = await fetch(`${baseUrl}/courses/${course_id}`);
   let course = await response.json();
@@ -12,5 +19,22 @@ export default async function Curso({
 }) {
   const { course_id } = await params;
   const course = await getCourse(course_id);
-  return <h1>{course.name}</h1>;
+  const classes = await getClassesFromCourse(course_id);
+
+  return (
+    <>
+      <h1>{course.name}</h1>
+      <h2>{course.course_code}</h2>
+      <p>{course.description}</p>
+      <img src={course.url_image} alt={course.name} className="h-50" />
+      {classes.map((clase: any) => {
+        return (
+          <div key={clase.class_id}>
+            <h3>{clase.title}</h3>
+            <p>{clase.description}</p>
+          </div>
+        );
+      })}
+    </>
+  );
 }
