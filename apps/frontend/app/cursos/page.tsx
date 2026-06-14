502e5183dc13d45cc3a7a3dd0725df7a8275ee76
Franz Nunez <kasparov01234@gmail.com>
2026-06-14 10:18:55 -0500

    feat: adding call to admin-courses and courses endpoint

--- apps/frontend/app/cursos/page.tsx
+++ apps/frontend/app/cursos/page.tsx
@@ -1,3 +1,31 @@
-export default function Cursos() {
-  return <h1>Cursos</h1>;
+const baseUrl = process.env.API_URL || 'http://localhost:3001';
+
+async function getCourseData(busqueda: string) {
+  console.log(busqueda);
+  let response = await fetch(`${baseUrl}/courses?q=${busqueda}`);
+  let courses = await response.json();
+  console.log(courses);
+  return courses;
+}
+export default async function Cursos({
+  searchParams,
+}: {
+  searchParams: Promise<{ busqueda: string }>;
+}) {
+  const { busqueda = '' } = await searchParams;
+  const courses = await getCourseData(busqueda);
+  return (
+    <section className="courses">
+      <h1>Courses</h1>
+      <input type="text" placeholder="Search courses..." />
+      {courses.data.map((course: any) => (
+        <div key={course.course_id}>
+          <h2>{course.name}</h2>
+          <h3>{course.course_code}</h3>
+          <p>{course.description}</p>
+          <img src={course.url_image} alt={course.name} className="h-50" />
+        </div>
+      ))}
+    </section>
+  );
 }
--- apps/frontend/app/dashboard/page.tsx
+++ apps/frontend/app/dashboard/page.tsx
@@ -1,3 +1,11 @@
+const baseUrl = process.env.API_URL || 'http://localhost:3001';
+
+async function getCourseData() {
+  let response = await fetch(`${baseUrl}/courses/dashboard`);
+  let courses = await response.json();
+  return courses;
+}
+
 export default function Dashboard() {
   return <h1>User Dashboard</h1>;
 }
