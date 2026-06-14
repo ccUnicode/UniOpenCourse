b6e2603b9094d85120e79619efa0040208fb59d6
Franz Nunez <kasparov01234@gmail.com>
2026-06-14 11:02:17 -0500

    feat: adding global search api call

--- apps/frontend/app/busqueda/page.tsx
+++ apps/frontend/app/busqueda/page.tsx
@@ -1,3 +1,26 @@
-export default function Busqueda() {
-  return <h1>Resultados de búsqueda</h1>;
+const API_URL = process.env.API_URL || 'http://localhost:3001';
+async function getBusqueda(busqueda: string) {
+  {
+    let response = await fetch(`${API_URL}/search?q=${busqueda}`);
+    let resultados = await response.json();
+    return resultados;
+  }
+}
+
+export default async function Busqueda({
+  searchParams,
+}: {
+  searchParams: Promise<{ busqueda: string }>;
+}) {
+  const { busqueda = '' } = await searchParams;
+  let resultados = await getBusqueda(busqueda);
+  let data = resultados.data;
+  return (
+    <>
+      <h1>Resultados de búsqueda</h1>
+      {data.map((resultado: any) => {
+        return <h1>resultado.title</h1>;
+      })}
+    </>
+  );
 }
