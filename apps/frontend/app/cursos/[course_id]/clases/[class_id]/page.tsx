d99c1111c8f63f06a0334fdea60dffa3afbd5391
Franz Nunez <kasparov01234@gmail.com>
2026-08-01 22:18:18 -0500

    fix: making embed the video_url

--- apps/frontend/app/cursos/[course_id]/clases/[class_id]/page.tsx
+++ apps/frontend/app/cursos/[course_id]/clases/[class_id]/page.tsx
@@ -1,43 +1,43 @@
 import { Material } from '@/interfaces/material.interface';
+import { getClassData, getMaterialData } from '@/services/classes.service';
+import { notFound } from 'next/navigation';
 
-const baseUrl = process.env.API_URL || 'http://localhost:3001';
-
-async function getMaterialData(class_id: string) {
-  const response = await fetch(`${baseUrl}/classes/${class_id}/materials`);
-  const materials = await response.json();
-  return materials;
-}
-
-async function getClassData(class_id: string) {
-  const response = await fetch(`${baseUrl}/classes/${class_id}`);
-  const clase = await response.json();
-  return clase;
-}
 export default async function Clase({
   params,
 }: {
   params: Promise<{ class_id: string }>;
 }) {
   const { class_id } = await params;
+  if (class_id != parseInt(class_id).toString()) {
+    notFound();
+  }
   const clase = await getClassData(class_id);
   const materials = await getMaterialData(class_id);
+  console.log(clase);
 
   return (
     <>
       <h1>{clase.title}</h1>
       <p>{clase.description}</p>
-      <iframe
-        width="560"
-        height="315"
-        src={clase.url_youtube} // Asegurar que el link sea youtube embed
-        title="YouTube video player"
-      ></iframe>
+      {clase.url_youtube ? (
+        <iframe
+          width="560"
+          height="315"
+          src={clase.url_youtube}
+          title="YouTube video player"
+        ></iframe>
+      ) : null}
+
       <h2>Materials</h2>
-      <ul>
-        {materials.map((material: Material) => (
-          <li key={material.material_id}>{material.class_id} </li>
-        ))}
-      </ul>
+      {materials.error ? (
+        <p>Error: {materials.message}</p>
+      ) : (
+        <ul>
+          {materials.map((material: Material) => (
+            <li key={material.material_id}>{material.class_id} </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
