61783020b351fbc90853027fe23500ea911e5374
Franz Nunez <kasparov01234@gmail.com>
2026-08-02 21:43:56 -0500

    fix: showing 7 pages max

--- apps/frontend/components/pagination.tsx
+++ apps/frontend/components/pagination.tsx
@@ -1,12 +1,37 @@
 import PaginationProps from '@/interfaces/pagination-props.interface';
 
+function getPages(totalPages: number, page: number): number[] {
+  const maxVisiblePages = 7;
+  const halfVisiblePages = Math.floor(maxVisiblePages / 2);
+
+  if (totalPages <= maxVisiblePages) {
+    return Array.from({ length: totalPages }).map((_, index) => index + 1);
+  }
+
+  // Bloque inicial
+  if (page <= halfVisiblePages + 1) {
+    return Array.from({ length: maxVisiblePages }).map((_, index) => index + 1);
+  }
+  // Bloque final
+  if (page >= totalPages - halfVisiblePages) {
+    return Array.from({ length: maxVisiblePages }).map(
+      (_, index) => totalPages - maxVisiblePages + index + 1,
+    );
+  }
+
+  // Caso intermedio
+  const startPage = Math.max(1, page - halfVisiblePages);
+
+  return Array.from({ length: maxVisiblePages }, (_, index) => startPage + index);
+}
+
 export function Pagination({ page, totalPages, onPageChange }: PaginationProps) {
   return (
     <div className="flex gap-4 mt-4 justify-center items-center">
       <button
         onClick={() => onPageChange(page - 1)}
-        disabled={page === 1}
-        className={`${page === 1 ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}
+        disabled={page <= 1}
+        className={`${page <= 1 ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -19,9 +44,7 @@ export function Pagination({ page, totalPages, onPageChange }: PaginationProps) 
         </svg>
       </button>
       <div className="flex gap-2">
-        {Array.from({ length: totalPages }, (_, index) => {
-          const pageNumber = index + 1;
-
+        {getPages(totalPages, page).map((pageNumber) => {
           return (
             <button
               key={pageNumber}
@@ -39,10 +62,10 @@ export function Pagination({ page, totalPages, onPageChange }: PaginationProps) 
       </div>
       <button
         className={`${
-          page === totalPages ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'
+          page >= totalPages ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'
         }`}
         onClick={() => onPageChange(page + 1)}
-        disabled={page === totalPages}
+        disabled={page >= totalPages}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
