c388c74a7f958523cbd5790c8d8d95755531078e
JefersonYat22 <jefersonjsy48@gmail.com>
2026-08-03 18:30:10 -0500

    chore(database): add backfill to migrate file names to file_path

--- apps/backend/prisma/migrations/20260802052526_add_material_filepath/migration.sql
+++ apps/backend/prisma/migrations/20260802052526_add_material_filepath/migration.sql
@@ -1,2 +1,6 @@
 -- AlterTable
 ALTER TABLE "Material" ADD COLUMN     "file_path" VARCHAR(255);
+
+UPDATE "Material" SET "file_path" = "url_link" WHERE "material_type" = 'file' AND "file_path" IS NULL;
+
+UPDATE "Material" SET "url_link" = NULL WHERE "material_type" = 'file';
