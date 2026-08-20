0d035decebe7bd117b2daec01e9d4c0d72edb3bf
Franz Nunez <kasparov01234@gmail.com>
2026-08-20 00:47:20 -0500

    fix: using config service for multer

--- apps/backend/src/courses/courses.module.ts
+++ apps/backend/src/courses/courses.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 import { CoursesController } from './courses.controller';
 import { AdminCoursesController } from './admin-courses.controller';
 import { CoursesService } from './courses.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { createStorageConfig } from 'src/utils/storage.config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    MulterModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const storageDir = configService.get<string>('STORAGE_PATH', './storage');
+
+        return {
+          storage: createStorageConfig(storageDir),
+        };
+      },
+    }),
+  ],
   controllers: [CoursesController, AdminCoursesController],
   providers: [CoursesService],
 })
